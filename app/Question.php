f51bbfdd43d2e5b7fc62a1b35c938712d2e34aa0
QuyHai <nguyenquyhai2002@gmail.com>
2019-09-09 21:13:54 +0700

    Question eloquent && migration

--- app/Question.php
+++ app/Question.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     //
+    protected $guard=[];
+
+    public function user()
+    {
+        return $this->belongsTo(App\User::class);
+    }
 }
--- database/migrations/2019_09_09_130343_create_questions_table.php
+++ database/migrations/2019_09_09_130343_create_questions_table.php
@@ -15,6 +15,17 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->text('content');
+            $table->bigInteger('votes')->default(0);
+            $table->unsignedBigInteger('views')->default(0);
+            $table->string('slug', 100)->unique()->nullable();
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('user_id')
+                    ->references('id')->on('users')
+                    ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
